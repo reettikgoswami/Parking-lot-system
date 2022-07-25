86d8ce65c4fe5f03caf095104d1a3d6c6b91951f
reettikgoswami <reettikgoswami1997@gmail.com>
2022-07-26 00:27:49 +0530

    Move diaplay functions to utilities

--- lib/parking_system.rb
+++ lib/parking_system.rb
@@ -1,8 +1,12 @@
+require 'active_support'
+
+require_relative 'utilities'
 require_relative 'parking_lot'
 require_relative 'car'
 require_relative 'ticket'
 
 class ParkingSystem
+  include Utilities
   attr_reader :parking_lot
 
   def initialize(total_parking_space)
@@ -11,35 +15,35 @@ class ParkingSystem
   end
 
   def park(reg_no, color)
-    available_slot = parking_lot.find_available_slot
-    if available_slot.present?
-      car = Car.new(reg_no, color)
-      parking_ticket = Ticket.new(car, available_slot)
-      parking_lot.allot_parking_space(parking_ticket, available_slot)
-      track_tickets[parking_ticket.id] = parking_ticket
-      print_parking_recite(parking_ticket)
-    else
+    available_slot = @parking_lot.find_available_slot
+    if available_slot.nil?
       p 'Parking lot is full.'
+    else
+      car = Car.new(reg_no, color)
+      parking_ticket = Ticket.new(car, available_slot, generate_unique_ticket_id)
+      parking_lot.allot_parking_space(parking_ticket, available_slot)
+      @track_tickets[parking_ticket.id] = parking_ticket
+      print_parking_recite(parking_ticket)
     end
   end
 
   def leave(parking_ticket_id)
-    unless track_tickets[parking_ticket_id].present?
+    parking_ticket = @track_tickets[parking_ticket_id]
+    unless parking_ticket.present?
       p 'Invalid parking ticket!'
       return
     end
     @track_tickets.delete(parking_ticket_id)
-    parking_spot parking_lot.leave_parking_space(parking_ticket_id)
-    p `Slot number #{parking_spot + 1} is free`
+    parking_spot = parking_ticket.parking_spot
+    @parking_lot.leave_parking_space(parking_spot)
+    p "Slot number #{parking_spot + 1} is free"
   end
 
   def find_registration_numbers_by_color(color)
     registration_numbers = parking_lot.filter_car_by_color(color)
-    registration_numbers.each do
-      reg_no
-      p registration_numbers
+    registration_numbers.each do |reg_no|
+      p reg_no
     end
-    # return  registration_numbers
   end
 
   def find_tickets_by_color(color)
@@ -54,18 +58,22 @@ class ParkingSystem
     parking_ticket = parking_lot.find_parking_ticket(reg_no)
     if parking_ticket.present?
       print_parking_recite(parking_ticket)
+      parking_ticket
     else
       p 'Parking ticket not found!'
     end
   end
 
+  def show_parking_lot
+    display_parking_lot(@parking_lot)
+  end
+
   private
 
-  def print_parking_recite(parking_ticket)
-    p '==============================================='
-    p `Allocated slot: #{parking_ticket.parking_slot}`
-    p `Parking ticket ID: #{parking_ticket.id}`
-    p `Registration number: #{parking_ticket.car.reg_no}`
-    p '==============================================='
+  def generate_unique_ticket_id
+    parking_ticket = loop do
+      ticket_token = generate_parking_ticket_id
+      break ticket_token unless @track_tickets.key?(ticket_token)
+    end
   end
 end
